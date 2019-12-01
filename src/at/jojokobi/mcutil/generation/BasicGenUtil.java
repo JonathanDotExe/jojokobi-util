33ea1d244ab1972d441049b1f18f5c144a2f2cfc
jojokobi <jhocheneder@gmx.at>
2019-12-01 19:51:16 +0100

    Added applyPhysics parameter to generateCube(...)

--- src/at/jojokobi/mcutil/generation/BasicGenUtil.java
+++ src/at/jojokobi/mcutil/generation/BasicGenUtil.java
@@ -66,17 +66,21 @@ public final class BasicGenUtil {
 	}
 	
 	public static void generateCube (Location place, Material block, Predicate<Block> replace, BlockModifier modifier, int width, int height, int length) {
+		generateCube(place, block, replace, modifier, width, height, length, true);
+	}
+	
+	public static void generateCube (Location place, Material block, Predicate<Block> replace, BlockModifier modifier, int width, int height, int length, boolean applyPhysics) {
 		for (int x = 0; x < width; x++) {
 			for (int y = 0; y < height; y++) {
 				for (int z = 0; z < length; z++) {
 					Location blockplace = new Location (place.getWorld(), x + place.getBlockX(), y + place.getBlockY(), z + place.getBlockZ());
 					if (replace.test(blockplace.getBlock())) {
-						blockplace.getBlock().setType(block);
+						blockplace.getBlock().setType(block, applyPhysics);
 						if (modifier != null) {
 							BlockState state = blockplace.getBlock().getState();
 							state.setType(block);
 							modifier.modify(state);
-							state.update(true);
+							state.update(true, applyPhysics);
 						}
 					}
 				}
