90652f3136ccd8bcfcaf5e498e885c16ba94543c
DatDonathan <jojo08082003@gmail.com>
2020-09-15 16:36:23 +0200

    Added option to use raytracing when using random AI

--- src/at/jojokobi/mcutil/entity/ai/RandomAroundPlaceTask.java
+++ src/at/jojokobi/mcutil/entity/ai/RandomAroundPlaceTask.java
@@ -3,6 +3,7 @@ package at.jojokobi.mcutil.entity.ai;
 import java.util.function.Function;
 
 import org.bukkit.Location;
+import org.bukkit.util.RayTraceResult;
 import org.bukkit.util.Vector;
 
 import at.jojokobi.mcutil.entity.CustomEntity;
@@ -17,18 +18,25 @@ public class RandomAroundPlaceTask implements EntityTask{
 	private double startDistance = 3;
 	private Function<CustomEntity<?>, Location> place;
 	private int timeout = 0;
+	private boolean raytrace = false;
+	private boolean yMove = true;
 	
 	public RandomAroundPlaceTask(Function<CustomEntity<?>, Location> place) {
-		this (place, 10, 10, 2);
+		this (place, 10, 10, 2, false, true);
 	}
 
-	public RandomAroundPlaceTask(Function<CustomEntity<?>, Location> place, double goalRange, double maxDistance, double startDistance) {
+
+	public RandomAroundPlaceTask(Function<CustomEntity<?>, Location> place, double goalRange, double maxDistance, 
+			double startDistance, boolean raytrace, boolean yMove) {
 		super();
 		this.goalRange = goalRange;
 		this.maxDistance = maxDistance;
 		this.startDistance = startDistance;
 		this.place = place;
+		this.raytrace = raytrace;
+		this.yMove = yMove;
 	}
+
 
 	@Override
 	public boolean canApply(CustomEntity<?> entity) {
@@ -39,10 +47,18 @@ public class RandomAroundPlaceTask implements EntityTask{
 	@Override
 	public Locatable apply(CustomEntity<?> entity) {
 		if (timeout <= 0) {
-			Vector direction = new Vector(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5);
+			Vector direction = new Vector(Math.random() - 0.5, yMove ? (Math.random() - 0.5) : 0, Math.random() - 0.5);
 			if (direction.length() != 0) {
 				direction.normalize();
-				direction.multiply((Math.random() + 0.5) * goalRange);
+				double range = Math.random() * goalRange;
+				direction.multiply(Math.random() < 0.5 ? -1 : 1);
+				if (raytrace) {
+					RayTraceResult rt = entity.getEntity().getWorld().rayTraceBlocks(entity.getEntity().getLocation().add(0, 1, 0), direction, range);
+					if (rt != null) {
+						range = rt.getHitPosition().distance(entity.getEntity().getLocation().add(0, 1, 0).toVector());
+					}
+				}
+				direction.multiply(range);
 			}
 			goal = entity.getEntity().getLocation().add(direction);
 			timeout = 24;
