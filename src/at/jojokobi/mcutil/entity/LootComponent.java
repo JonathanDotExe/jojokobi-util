a9357c0073acf07fa68b489c507c82bb00f9e2ff
DatDonathan <jojo08082003@gmail.com>
2021-04-15 14:06:26 +0200

    Fixed double drops

--- src/at/jojokobi/mcutil/entity/LootComponent.java
+++ src/at/jojokobi/mcutil/entity/LootComponent.java
@@ -30,7 +30,7 @@ public class LootComponent implements EntityComponent {
 	public void onDamage(CustomEntity<?> entity, EntityDamageEvent event) {
 		EntityComponent.super.onDamage(entity, event);
 		HealthComponent health = entity.getComponent(HealthComponent.class);
-		if (health.getHealth(entity) - event.getFinalDamage() <= 0.5) {
+		if (health.getHealth(entity) - event.getFinalDamage() < 0.5) {
 			for (ItemStack stack : loot.populateLoot(new Random(), null)) {
 				entity.getEntity().getWorld().dropItemNaturally(entity.getEntity().getLocation(), stack);
 			}
