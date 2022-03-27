46440c56dba5d02e3b7d3ab2bc7c5e6b0541d475
DatDonathan <jojo08082003@gmail.com>
2022-03-27 20:08:23 +0200

    Only counting custom entities in spawner

--- src/main/java/at/jojokobi/mcutil/entity/spawns/CustomEntitySpawner.java
+++ src/main/java/at/jojokobi/mcutil/entity/spawns/CustomEntitySpawner.java
@@ -5,26 +5,35 @@ import java.util.List;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.util.Vector;
+
+import at.jojokobi.mcutil.entity.CustomEntity;
+import at.jojokobi.mcutil.entity.EntityHandler;
 
 public class CustomEntitySpawner {
 	
 	private List<CustomEntitySpawnData> entities = new ArrayList<CustomEntitySpawnData>();
 
-	public CustomEntitySpawner(Plugin plugin) {
+	public CustomEntitySpawner(Plugin plugin, EntityHandler handler) {
 		Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, () -> {
+			System.out.println("Spawn loop");
 			//Spawn algorithm
 			for (Player player : Bukkit.getOnlinePlayers()) {
+				System.out.println("Player " + player);
 				for (CustomEntitySpawnData data : entities) {
-					int count = (int) player.getNearbyEntities(data.getMaxPlayerDistance(), 512, data.getMaxPlayerDistance()).stream().filter(e -> e instanceof LivingEntity).count();
+					int count = (int) player.getNearbyEntities(data.getMaxPlayerDistance(), 512, data.getMaxPlayerDistance()).stream().filter(e -> {
+						CustomEntity<?> c = handler.getCustomEntityForEntity(e);
+						return c != null && !c.isSave();
+					}).count();
+					System.out.println("Custom Entitiy " + data.getSpawn().getIdentifier() + ", Count " + count);
 					//Spawn
 					for (int i = 0; i < data.getTries(); i++) {
+						System.out.println("Try");
 						if (count < data.getMaxEntitiesAround() && Math.random() < data.getChance() && data.canSpawn(player)) {
-							double x = Math.random() * (data.getMaxPlayerDistance() - data.getMinPlayerDistance()) * Math.signum(Math.random() - 0.5);
-							double z = Math.random() * (data.getMaxPlayerDistance() - data.getMinPlayerDistance()) * Math.signum(Math.random() - 0.5);
+							double x = (Math.random() * (data.getMaxPlayerDistance() - data.getMinPlayerDistance()) + data.getMinPlayerDistance()) * Math.signum(Math.random() - 0.5);
+							double z = (Math.random() * (data.getMaxPlayerDistance() - data.getMinPlayerDistance()) + data.getMinPlayerDistance()) * Math.signum(Math.random() - 0.5);
 							
 							Vector vec = new Vector(x, 0, z);
 							Location loc = player.getLocation().add(vec);
