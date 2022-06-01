d1f4fd0916cc88dbcf25600559876d4aa4b6134d
DatDonathan <jojo08082003@gmail.com>
2022-06-01 22:39:09 +0200

    Added build function to building

--- src/main/java/at/jojokobi/mcutil/building/Building.java
+++ src/main/java/at/jojokobi/mcutil/building/Building.java
@@ -2,13 +2,27 @@ package at.jojokobi.mcutil.building;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiConsumer;
 
+import org.bukkit.Location;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 
 public class Building implements ConfigurationSerializable{
 
 	private List<BuildingBlock> blocks;
 	private List<BuildingMark> marks;
+	
+	
+	public void build(Location loc, BiConsumer<Location, String> markInterpreter, boolean physicsUpdate) {
+		//Blocks
+		for (BuildingBlock block : blocks) {
+			loc.getBlock().getRelative(block.getX(), block.getY(), block.getZ()).setBlockData(block.getBlock(), physicsUpdate);
+		}
+		//Marks
+		for (BuildingMark mark : marks) {
+			markInterpreter.accept(loc.clone().add(mark.getX(), mark.getY(), mark.getZ()), mark.getMark());
+		}
+	}
 
 	@Override
 	public Map<String, Object> serialize() {
