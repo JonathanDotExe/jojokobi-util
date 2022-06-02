ceb02b84b952911018bb4b98df98de76e5e7eb30
DatDonathan <jojo08082003@gmail.com>
2022-06-02 14:56:19 +0200

    Made building serializeable

--- src/main/java/at/jojokobi/mcutil/building/Building.java
+++ src/main/java/at/jojokobi/mcutil/building/Building.java
@@ -1,17 +1,33 @@
 package at.jojokobi.mcutil.building;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiConsumer;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Sign;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
+
+import at.jojokobi.mcutil.TypedMap;
 
 public class Building implements ConfigurationSerializable{
 
-	private List<BuildingBlock> blocks;
-	private List<BuildingMark> marks;
+	private List<BuildingBlock> blocks = new ArrayList<BuildingBlock>();
+	private List<BuildingMark> marks = new ArrayList<BuildingMark>();
 	
+	
+	public void buildWithMarkSigns(Location loc, boolean physicsUpdate) {
+		build(loc, (place, str) -> {
+			place.getBlock().setType(Material.OAK_SIGN);
+			Sign sign = (Sign) place.getBlock().getState();
+			sign.setLine(0, "####");
+			sign.setLine(1, str);
+			sign.setLine(3, "####");
+		}, physicsUpdate);
+	}
 	
 	public void build(Location loc, BiConsumer<Location, String> markInterpreter, boolean physicsUpdate) {
 		//Blocks
@@ -26,7 +42,19 @@ public class Building implements ConfigurationSerializable{
 
 	@Override
 	public Map<String, Object> serialize() {
-		return null;
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("blocks", blocks);
+		map.put("marks", marks);
+		return map;
+	}
+	
+	public static Building deserialize(Map<String, Object> map) {
+		TypedMap m = new TypedMap(map);
+		Building building = new Building();
+		building.blocks.addAll(m.getList("blocks", BuildingBlock.class));
+		building.marks.addAll(m.getList("marks", BuildingMark.class));
+
+		return building;
 	}
 	
 	
