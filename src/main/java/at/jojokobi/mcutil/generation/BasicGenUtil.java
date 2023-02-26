68b39d83b62351d9b2249a68eddc65fbc6b92094
DatDonathan <jojo08082003@gmail.com>
2023-02-26 18:41:16 +0100

    Change rotation direction

--- src/main/java/at/jojokobi/mcutil/generation/BasicGenUtil.java
+++ src/main/java/at/jojokobi/mcutil/generation/BasicGenUtil.java
@@ -56,42 +56,42 @@ public final class BasicGenUtil {
 		switch (face) {
 		case DOWN:
 			return BlockFace.DOWN;
-		case EAST:
-			return BlockFace.SOUTH;
-		case EAST_NORTH_EAST:
-			return BlockFace.SOUTH_SOUTH_EAST;
-		case EAST_SOUTH_EAST:
-			return BlockFace.SOUTH_SOUTH_WEST;
-		case NORTH:
+		case SOUTH:
 			return BlockFace.EAST;
-		case NORTH_EAST:
-			return BlockFace.SOUTH_EAST;
-		case NORTH_NORTH_EAST:
-			return BlockFace.EAST_SOUTH_EAST;
-		case NORTH_NORTH_WEST:
+		case SOUTH_SOUTH_EAST:
 			return BlockFace.EAST_NORTH_EAST;
-		case NORTH_WEST:
+		case SOUTH_SOUTH_WEST:
+			return BlockFace.EAST_SOUTH_EAST;
+		case EAST:
+			return BlockFace.NORTH;
+		case SOUTH_EAST:
 			return BlockFace.NORTH_EAST;
+		case EAST_SOUTH_EAST:
+			return BlockFace.NORTH_NORTH_EAST;
+		case EAST_NORTH_EAST:
+			return BlockFace.NORTH_NORTH_WEST;
+		case NORTH_EAST:
+			return BlockFace.NORTH_WEST;
 		case SELF:
 			return BlockFace.SELF;
-		case SOUTH:
-			return BlockFace.WEST;
-		case SOUTH_EAST:
-			return BlockFace.SOUTH_WEST;
-		case SOUTH_SOUTH_EAST:
-			return BlockFace.WEST_SOUTH_WEST;
-		case SOUTH_SOUTH_WEST:
-			return BlockFace.WEST_NORTH_WEST;
+		case WEST:
+			return BlockFace.SOUTH;
 		case SOUTH_WEST:
-			return BlockFace.NORTH_WEST;
+			return BlockFace.SOUTH_EAST;
+		case WEST_SOUTH_WEST:
+			return BlockFace.SOUTH_SOUTH_EAST;
+		case WEST_NORTH_WEST:
+			return BlockFace.SOUTH_SOUTH_WEST;
+		case NORTH_WEST:
+			return BlockFace.SOUTH_WEST;
 		case UP:
 			return BlockFace.UP;
-		case WEST:
-			return BlockFace.NORTH;
-		case WEST_NORTH_WEST:
-			return BlockFace.NORTH_NORTH_EAST;
-		case WEST_SOUTH_WEST:
-			return BlockFace.NORTH_NORTH_WEST;
+		case NORTH:
+			return BlockFace.WEST;
+		case NORTH_NORTH_EAST:
+			return BlockFace.WEST_NORTH_WEST;
+		case NORTH_NORTH_WEST:
+			return BlockFace.WEST_SOUTH_WEST;
 		}
 		return face;
 	}
