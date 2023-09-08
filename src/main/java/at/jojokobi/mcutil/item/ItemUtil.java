04d66221ff6fec270a7694e15ffb6a982c735d28
DatDonathan <jojo08082003@gmail.com>
2023-09-08 19:30:28 +0200

    Update to 1.20.1

--- src/main/java/at/jojokobi/mcutil/item/ItemUtil.java
+++ src/main/java/at/jojokobi/mcutil/item/ItemUtil.java
@@ -1,6 +1,6 @@
 package at.jojokobi.mcutil.item;
 
-import org.bukkit.craftbukkit.v1_19_R3.inventory.CraftItemStack;
+import org.bukkit.craftbukkit.v1_20_R1.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
